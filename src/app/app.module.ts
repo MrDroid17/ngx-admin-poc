42d4c19e4bec350a0f10a8207df87e95a4db047b
Sobhit Kumar <sobhit2010@gmail.com>
2018-07-02 13:08:45 +0530

    Basic ngx-skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NbThemeModule } from '@nebular/theme';
+import { RouterModule } from '@angular/router'; // we also need angular router for Nebular to function properly
+import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  {path : '' ,component: DashboardComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { useHash: true }),
+    NbSidebarModule,
+    NbLayoutModule,
+    NbThemeModule.forRoot({ name: 'default' })
   ],
-  providers: [],
+  providers: [NbSidebarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
